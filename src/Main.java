a5936d7c21a94cd14e9092d9dd44df1bf26d567b
PiGru08 <grucar77@gmail.com>
2024-09-17 19:49:05 +0200

    liczbę palindromiczna

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,61 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String slowo = wpiszLiczbyZKlawiatury();
+        // The variable i was not declared, removed it as it serves no purpose here.
+
+        String slowo = wpiszStringZKlawiatury();
         boolean palindrom = palindrom(slowo);
-        if(palindrom){
-            System.out.println("tak jest palindronem");
-        }else{
-            System.out.println("Nie jest palindronem");
+
+        if (palindrom) {
+            System.out.println("Tak, jest palindromem.");
+        } else {
+            System.out.println("Nie jest palindromem.");
         }
+
+        Integer liczba = wpiszLiczbeZKlawiatury();
+        Integer liczbaNajblizszaPalindroma = najblizszaDoPalindroma(liczba);
+        System.out.println("Najbliższa liczba palindromowa to: " + liczbaNajblizszaPalindroma);
     }
-    private static String wpiszLiczbyZKlawiatury(){
+
+    private static String wpiszStringZKlawiatury() {
         System.out.println("Podaj słowo: ");
         Scanner Klawiatura = new Scanner(System.in);
-        String slowo = Klawiatura.nextLine();
-        return slowo;
+        return Klawiatura.nextLine();
     }
-    private static boolean palindrom(String slowo){
-        boolean palindrom = true;
-        for (int i = 0,j = slowo.length()-1; i < slowo.length()/2; i++,j--) {
-            if(slowo.charAt(i) != slowo.charAt(j)){
-                palindrom = false;
-                return palindrom;
+
+    private static Integer wpiszLiczbeZKlawiatury() {
+        System.out.println("Podaj liczbę: ");
+        Scanner Klawiatura = new Scanner(System.in);
+        return Klawiatura.nextInt();
+    }
+
+    private static boolean palindrom(String slowo) {
+        for (int i = 0, j = slowo.length() - 1; i < j; i++, j--) {
+            if (slowo.charAt(i) != slowo.charAt(j)) {
+                return false;
             }
         }
-        return palindrom;
+        return true;
+    }
+
+    private static Integer najblizszaDoPalindroma(Integer liczba) {
+        int mniejsza = liczba - 1;
+        int wieksza = liczba + 1;
+
+        if (palindrom(Integer.toString(liczba))) {
+            return liczba;
+        }
+
+        while (true) {
+            if (palindrom(Integer.toString(mniejsza))) {
+                return mniejsza;
+            }
+            if (palindrom(Integer.toString(wieksza))) {
+                return wieksza;
+            }
+            mniejsza--;
+            wieksza++;
+        }
     }
 }
